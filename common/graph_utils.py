665f5bc75677c2c84d325532d1d506211214351a
dani k <kogan.dani1@gmail.com>
2024-01-30 00:21:21 +0200

    Added subgraph enumeration code

--- common/graph_utils.py
+++ common/graph_utils.py
@@ -1,12 +1,21 @@
 import itertools
 import random
+from os import cpu_count
 import networkx as nx
+from joblib import Parallel, delayed
+from tqdm import tqdm
+from common.logger import TimeLogging
+from common.parallel_computation import tqdm_joblib
 
 
 def relabel_graph_nodes_by_contiguous_order(g: nx.Graph, copy):
     nodes_map = dict(zip(list(g.nodes), [i for i in range(len(g))]))
     graph = nx.relabel_nodes(g, nodes_map, copy=copy)
     return graph
+
+
+def fill_subgraph_missing_nodes(subgraph, start_graph):
+    subgraph.add_nodes_from(list(start_graph.nodes))
 
 
 class GED_graph_generator:
@@ -73,3 +82,74 @@ class GED_graph_generator:
             copy_graph = generate_operations_series_list(self.reference_graph, ged_dist_operations_list)
             yield copy_graph
         print("finished generation")
+
+
+class SubGraphGenerator:
+
+    @staticmethod
+    def generate_subgraph(graph, connected_subgraphs_nodes_list):
+        subgraph = graph.copy()
+
+        removed_nodes = [node for node in graph.nodes if node not in connected_subgraphs_nodes_list]
+        subgraph.remove_nodes_from(removed_nodes)
+        fill_subgraph_missing_nodes(subgraph, graph)
+
+        return subgraph
+
+    @staticmethod
+    def generate_subgraph_for_sublists_of_nodes(graph, connected_subgraphs_nodes_lists):
+        return [SubGraphGenerator.generate_subgraph(graph, connected_subgraphs_nodes_list)
+                for connected_subgraphs_nodes_list in connected_subgraphs_nodes_lists]
+
+    # https://stackoverflow.com/questions/75727217/fast-way-to-find-all-connected-subgraphs-of-given-size-in-python
+    @staticmethod
+    def all_connected_subgraphs(g, m):
+        found_subgraphs = []
+        n = len(g.nodes)
+        adj_sets = [set() for i in range(n)]
+        for (i, j) in g.edges:
+            adj_sets[i].add(j)
+            adj_sets[j].add(i)
+
+        def _recurse(t, possible, excluded, found_subgraphs=[]):
+            if len(t) == m:
+                found_subgraphs.append(t)
+                return
+            else:
+                excluded = set(excluded)
+                for i in possible:
+                    if i not in excluded:
+                        new_t = (*t, i)
+                        new_possible = possible | set(g[i].keys())
+                        excluded.add(i)
+                        _recurse(new_t, new_possible, excluded, found_subgraphs)
+
+        excluded = set()
+        for (i, possible) in enumerate(adj_sets):
+            excluded.add(i)
+            _recurse((i,), possible, excluded, found_subgraphs)
+        return found_subgraphs
+
+    @staticmethod
+    def generate_k_subgraphs(graph, k, chunk_size = 8_000):
+        curr_time = TimeLogging.log_time(None, "enter generate_k_subgraphs")
+
+        all_connected_subgraphs_nodes_lists = SubGraphGenerator.all_connected_subgraphs(graph, k)
+        n = len(all_connected_subgraphs_nodes_lists)
+
+        curr_time = TimeLogging.log_time(curr_time, f"finished all_connected_subgraphs (total of {n} graphs)")
+
+        chunks = [all_connected_subgraphs_nodes_lists[x:min(x + chunk_size, n)] for x in
+                  range(0, len(all_connected_subgraphs_nodes_lists), chunk_size)]
+        _ = TimeLogging.log_time(curr_time, f"finished splitting graphs list into {len(chunks)} chunks")
+
+        with tqdm_joblib(tqdm(desc="Graphs construction", total=len(chunks))) as progress_bar:
+            subgraph_lists_list = Parallel(n_jobs=int(cpu_count()), prefer='processes')(
+                delayed(SubGraphGenerator.generate_subgraph_for_sublists_of_nodes)
+                (graph=graph, connected_subgraphs_nodes_lists=connected_subgraphs_nodes_lists)
+                for connected_subgraphs_nodes_lists in chunks
+            )
+
+        subgraphs_list = [e for lst in subgraph_lists_list for e in lst]
+
+        return subgraphs_list
