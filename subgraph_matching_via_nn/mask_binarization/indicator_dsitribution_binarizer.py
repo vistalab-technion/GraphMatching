67eb12146651d97d4b3fb9c15b6671d7d1660399
dani k <kogan.dani1@gmail.com>
2024-07-08 03:44:03 +0300

    Added condition to bypass edge mask debugging code in case we are not using line graphs

--- subgraph_matching_via_nn/mask_binarization/indicator_dsitribution_binarizer.py
+++ subgraph_matching_via_nn/mask_binarization/indicator_dsitribution_binarizer.py
@@ -56,6 +56,11 @@ class IndicatorDistributionBinarizer:
     @staticmethod
     def debug_mask_scores(sub_graph, processed_sub_graph, chosen_nodes_indices, step_number,
                           current_chosen_subgraph_node_index, edge_scores_lists):
+
+        if not processed_sub_graph.is_line_graph:
+            print("Skipping debug_mask_scores, as it is not supported for non line graphs")
+            return
+
         # debug scores visually
         all_processed_nodes = list(processed_sub_graph.G.nodes)
         chosen_nodes = [all_processed_nodes[i] for i in chosen_nodes_indices + [current_chosen_subgraph_node_index]]
